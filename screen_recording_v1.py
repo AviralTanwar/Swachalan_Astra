efeb9decef9667a3830215ad9383521e48387ec0
AviralTanwar <aviraltanwar@gmail.com>
2025-01-06 01:52:04 +0530

    screen record code commit

--- screen_recording_v1.py
+++ screen_recording_v1.py
@@ -1,2 +1,64 @@
 # .\astra_venv\Scripts\activate 
 
+# Screen recording using OPENCV (GFG) 
+# Needs to have following changes:-
+#  1) In a new folder it needs to be created. If the folder doesn't exist, it will be created
+#  2) Each recording needs to be new. WIth name recording_{count}.s_astra. 
+#  3) In the final code, it will be in the folder -> program_files (windows) or in LINUX (wherever application files are)
+
+
+
+# importing the required packages
+import pyautogui
+import cv2
+import numpy as np
+
+# Specify resolution
+resolution = (1920, 1080)
+
+# Specify video codec
+codec = cv2.VideoWriter_fourcc(*"XVID")
+
+# Specify name of Output file
+filename = "Recording.avi"
+
+# Specify frames rate. We can choose any 
+# value and experiment with it
+fps = 60.0
+
+
+# Creating a VideoWriter object
+out = cv2.VideoWriter(filename, codec, fps, resolution)
+
+# Create an Empty window
+cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+
+# Resize this window
+cv2.resizeWindow("Live", 480, 270)
+
+while True:
+	# Take screenshot using PyAutoGUI
+	img = pyautogui.screenshot()
+
+	# Convert the screenshot to a numpy array
+	frame = np.array(img)
+
+	# Convert it from BGR(Blue, Green, Red) to
+	# RGB(Red, Green, Blue)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	# Write it to the output file
+	out.write(frame)
+	
+	# Optional: Display the recording screen
+	cv2.imshow('Live', frame)
+	
+	# Stop recording when we press 'q'
+	if cv2.waitKey(1) == ord('q'):
+		break
+
+# Release the Video writer
+out.release()
+
+# Destroy all windows
+cv2.destroyAllWindows()
